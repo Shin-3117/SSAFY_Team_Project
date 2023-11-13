01c709d9b24eda8d975ac971ff39367ef00b6d94
Shin-3117 <tlswnd3127@gmail.com>
2023-11-13 16:12:47 +0900

    add map api 카카오 맵 api를 Vue3 <script setup>문법으로 변경

--- Front-end/src/components/Map/Map.vue
+++ Front-end/src/components/Map/Map.vue
@@ -1,52 +1,44 @@
 <template>
   <div>
     <div id="map"></div>
-    <div class="button-group">
-      <button @click="changeSize(0)">Hide</button>
-      <button @click="changeSize(400)">show</button>
-      <button @click="displayMarker(markerPositions1)">marker set 1</button>
-      <button @click="displayMarker(markerPositions2)">marker set 2</button>
-      <button @click="displayMarker([])">marker set 3 (empty)</button>
-      <button @click="displayInfoWindow">infowindow</button>
-    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted, toRaw } from "vue";
-import config from '../../config/index.ts'
-const markerPositions1 = ref( [
-        [33.452278, 126.567803],
-        [33.452671, 126.574792],
-        [33.451744, 126.572441],
-      ])
-const markerPositions2 = ref([
-        [37.499590490909185, 127.0263723554437],
-        [37.499427948430814, 127.02794423197847],
-        [37.498553760499505, 127.02882598822454],
-        [37.497625593121384, 127.02935713582038],
-        [37.49629291770947, 127.02587362608637],
-        [37.49754540521486, 127.02546694890695],
-        [37.49646391248451, 127.02675574250912],
-      ])
-const markers = ref([])
-const infowindow = ref(null)
+<script setup>
+import { ref, onMounted } from 'vue';
+import config from '../../config/index.ts';
 
-const self = this
+let map = null;
+
 onMounted(() => {
-    if (window.kakao && window.kakao.maps) {
-      self.initMap();
-    } else {
-      const script = document.createElement("script");
-      /* global kakao */
-      script.onload = () => kakao.maps.load(self.initMap);
-      script.src =
-        `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${config.mapKey}`;
-      document.head.appendChild(script);
-    }
-  })
+  if (window.kakao && window.kakao.maps) {
+    initMap();
+  } else {
+    const script = document.createElement('script');
+    /* global kakao */
+    script.onload = () => kakao.maps.load(initMap);
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${config.mapKey}`;
+    document.head.appendChild(script);
+  }
+});
+
+const initMap = () => {
+  const container = document.getElementById('map');
+  const options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 5,
+  };
+
+  // 지도 객체를 등록합니다.
+  // 지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+  map = new kakao.maps.Map(container, options);
+};
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#map {
+  width: 400px;
+  height: 400px;
+}
 </style>
--- Front-end/src/views/MapView.vue
+++ Front-end/src/views/MapView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import KakaoMap from '../components/Map/KakaoMap.vue'
+// import KakaoMap from '../components/Map/KakaoMap.vue'
+import Map from '../components/Map/Map.vue'
 </script>
 
 <template>
   <main>
     <h1>지도</h1>
-    <KakaoMap/>
+    <Map/>
   </main>
 </template>
 
