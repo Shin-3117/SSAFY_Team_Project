ef614c62fc99c201be3da965af018fac2ced42d1
Shin-3117 <tlswnd3127@gmail.com>
2023-11-14 17:16:58 +0900

    merge

---
+++ Front-end/src/components/Exchange/Exchange.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{exchangeData}}
+  </div>
+</template>
+
+<script setup lang="ts">
+import getExchange from '@/api/Exchange'
+import { ref, onMounted } from 'vue';
+
+const exchangeData = ref(null);
+const isLoading = ref(true);
+onMounted(async () => {
+  try {
+    const response = await getExchange();
+    exchangeData.value = response;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- Front-end/src/views/ExchangeRateView.vue
+++ Front-end/src/views/ExchangeRateView.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import Exchange from '@/components/Exchange/Exchange.vue';
+
+</script>
+
 <template>
   <main>
     <h1>환율</h1>
+    <Exchange/>
   </main>
 </template>
 
