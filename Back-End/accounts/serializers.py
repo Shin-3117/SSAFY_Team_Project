a8484b04dce39036746bede51fb722c3e63d7906
sojugood <tkddlrtla@naver.com>
2023-11-16 02:01:12 +0900

    11.15

--- Back-End/accounts/serializers.py
+++ Back-End/accounts/serializers.py
@@ -1,9 +1,8 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
-from .models import User
-# from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
-# User = get_user_model()
+User = get_user_model()
 
 
 class CustomRegisterSerializer(RegisterSerializer):
