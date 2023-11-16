b9fef7fc064ad8108beac8bf8577f08405384854
unknown <tkddlrtla@naver.com>
2023-11-16 09:34:17 +0900

    11.16_start

--- Back-End/finlife/views.py
+++ Back-End/finlife/views.py
@@ -217,3 +217,69 @@ def deposit_options(request):
 @api_view(['GET'])
 def saving_options(request):
     pass
+
+'''
+# 2금융권 예금 API 요청 -> DB 저장
+# 다중 페이지 로드 및 저장 구현 확인
+@api_view(['GET'])
+def save_deposit2(request):
+    try:
+        N = 1
+        F = 2
+        while N <= F:
+            url = BASE_URL + DEPOSIT_URL
+            params = {
+                'auth': API_KEY,
+                'topFinGrpNo': '030300',
+                'pageNo': N
+            }
+            print(N)
+            response = requests.get(url, params=params).json()
+            F = response.get('result').get('max_page_no')
+            baseList = response.get('result').get('baseList')
+            optionList = response.get('result').get('optionList')
+
+            # 상품 데이터 저장
+            for product_data in baseList:
+                if product_data['join_way'] is None:
+                    continue
+                # 중복 저장 방지
+                if DepositProducts.objects.filter(fin_prdt_cd=product_data['fin_prdt_cd']).exists():
+                    continue
+                # 중복이 아닌 경우에만 유효성 검사 실시 및 데이터 저장
+                product_serializer = DepositProductsSerializer(data=product_data)
+                if product_serializer.is_valid(raise_exception=True):
+                    product_serializer.save()
+            
+            # 옵션 데이터 저장
+            for option_data in optionList:
+                # 왜래키로 지정한 필드 찾기
+                f_key = DepositProducts.objects.filter(fin_prdt_cd=option_data['fin_prdt_cd']).first()
+                # 연관된 DepositProducts 레코드가 없는 경우 건너뜀
+                if f_key is None:
+                    continue
+                # 저축 금리 null인 경우 저장하지 않음(해당 옵션 존재하지 않는 상품)
+                if option_data['intr_rate'] is None:
+                    continue
+                # 중복 저장 방지
+                if DepositOptions.objects.filter(
+                    intr_rate=option_data['intr_rate'],
+                    intr_rate2=option_data['intr_rate2'],
+                    save_trm=option_data['save_trm'],
+                    fin_prdt_cd_id=f_key.pk
+                ).exists():
+                    continue
+                # 중복이 아닌 경우에만 유효성 검사 실시 및 데이터 저장
+                option_serializer = DepositOptionsSerializer(data=option_data)
+                if option_serializer.is_valid(raise_exception=True):
+                    option_serializer.save(fin_prdt_cd=f_key)
+            N += 1
+        return JsonResponse({ 'message': 'okay' })
+    
+    # API 요청 실패 시 처리
+    except requests.RequestException as e:
+        return JsonResponse({"error": str(e)}, status=500)
+    # 기타 예외 처리
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+'''
