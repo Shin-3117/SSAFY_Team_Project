7cdd27914c9d67ec94a9dcd99f1abfebc792127e
unknown <tkddlrtla@naver.com>
2023-11-16 17:16:57 +0900

    11.16_update_re

--- Back-End/financial_project/settings.py
+++ Back-End/financial_project/settings.py
@@ -80,6 +80,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'REGISTER_SERIALIZER': 'accounts.serializers.CustomRegisterSerializer',
+    # Pagenation
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
--- Back-End/finlife/admin.py
+++ Back-End/finlife/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DepositOptions, DepositProducts, SavingOptions, SavingProducts, DepositSubscription, SavingsSubscription
 
-# Register your models here.
+admin.site.register(DepositOptions)
+admin.site.register(DepositProducts)
+admin.site.register(SavingOptions)
+admin.site.register(SavingProducts)
+admin.site.register(DepositSubscription)
+admin.site.register(SavingsSubscription)
